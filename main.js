88d649054a97081f5b1853a788b7b4593118789a
Codedzephyr <59132499+Codedzephyr@users.noreply.github.com>
2021-07-28 06:35:34 +0100

    feat:pages

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-var i = 0;
-var txt = "HELLO I'M HAMID";
-
-var speed = 500;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("intro").innerHTML = txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-typeWriter();
-
 const click = document.getElementById("navigation");
 const NavigationHeader = document.getElementById("start");
 
