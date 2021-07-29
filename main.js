3b51235bcadda17cf0daf50cc417bb1587335aef
Codedzephyr <59132499+Codedzephyr@users.noreply.github.com>
2021-07-29 21:43:59 +0100

    feat:pages

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ const NavigationHeader = document.getElementById("start");
 click.addEventListener("click", () => {
   NavigationHeader.classList.toggle("display");
 });
+
+//Get the button
+var mybutton = document.getElementById("my-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
